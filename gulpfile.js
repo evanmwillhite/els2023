b973549124c9745c6a4fea1ff3e021b17847bdac
Evan Willhite <ewillhite@activecampaign.com>
2023-03-09 14:08:25 -0600

    https fix

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ function watchTask(){
 
 // Fix Paths for Production URL.
 function pathFixes(){
-  const ghPages = '$1http://extremelawnservice.com/';
+  const ghPages = '$1https://extremelawnservice.com/';
   return src('dist/**/*.html')
     .pipe(replace(/("|'?)\/?styles\//g,  ghPages + 'styles/'))
     .pipe(replace(/("|'?)\/?scripts\//g, ghPages + 'scripts/'))
